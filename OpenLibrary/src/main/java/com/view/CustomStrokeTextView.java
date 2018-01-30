bf3dc9d2cee532907a28faf6c15b453e17058b65
NiceStars <nicestars@qq.com>
2018-01-30 11:33:09 +0800

    “提交代码”

---
+++ OpenLibrary/src/main/java/com/view/CustomStrokeTextView.java
@@ -0,0 +1,84 @@
+package com.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.openlibraryview.R;
+
+
+/**
+ * Created by nicestars on 2018/1/29 15:22.
+ * E-mail:nicestars.cn@gmail.com
+ */
+
+public class CustomStrokeTextView extends TextView {
+    private TextView textview = null;
+    private TypedArray typedArray;
+
+    public CustomStrokeTextView(Context context) {
+        super(context);
+        textview = new TextView(context);
+        typedArray = context.obtainStyledAttributes(R.styleable.CustomStrokeTextView);
+        init(typedArray);
+    }
+
+    public CustomStrokeTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        textview = new TextView(context, attrs);
+        typedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomStrokeTextView);
+        init(typedArray);
+    }
+
+    public CustomStrokeTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        textview = new TextView(context, attrs, defStyleAttr);
+        typedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomStrokeTextView);
+        init(typedArray);
+    }
+
+
+    private void init(TypedArray typedArray) {
+        Paint paint = textview.getPaint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(typedArray.getDimension(R.styleable.CustomStrokeTextView_outlineStrokeWidth, 0));
+        textview.setTextColor(typedArray.getColor(R.styleable.CustomStrokeTextView_outlineColor, 0));
+        textview.setGravity(getGravity());
+
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        CharSequence text = textview.getText();
+        if (text == null || !text.equals(this.getText())) {
+            textview.setText(text);
+            postInvalidate();
+        }
+        textview.measure(widthMeasureSpec, heightMeasureSpec);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        textview.layout(top, left, right, bottom);
+        if (changed){
+            getPaint().setShader(new LinearGradient(
+                    0, 0, 0, getHeight(),
+                    typedArray.getColor(R.styleable.CustomStrokeTextView_topcolor,0), typedArray.getColor(R.styleable.CustomStrokeTextView_bottomcolor,0), Shader.TileMode.CLAMP));
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        textview.draw(canvas);
+        super.onDraw(canvas);
+    }
+}
