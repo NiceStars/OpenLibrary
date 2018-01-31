1fe7b0bb14b4a209f9b0972afe9b1d931344c1c0
NiceStars <nicestars@qq.com>
2018-01-31 16:20:28 +0800

    fork了一下   https://github.com/xch168/StrokeTextView的代码  并写上了自己需要的功能   就是字体线性变色

--- app/src/main/java/com/nsopenlibrary/MainActivity.java
+++ app/src/main/java/com/nsopenlibrary/MainActivity.java
@@ -3,18 +3,15 @@ package com.nsopenlibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.view.CustomStrokeTextView;
-
 public class MainActivity extends AppCompatActivity {
 
-    private CustomStrokeTextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.tv_game_name1);
 
-        textView.setText("222");
+
     }
 }
